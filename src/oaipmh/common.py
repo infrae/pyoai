e44257dcb0345e7fbf628decb290e3350a1c91c2
faassen@882ca7fe-3cf8-0310-95c5-f9aabef819dd <none@none>
2005-06-02 12:47:34 +0000

    Separate reusable simple content classes into a common module. The server module will also start using this.
    --HG--
    extra : convert_revision : svn%3A882ca7fe-3cf8-0310-95c5-f9aabef819dd/pyoai/trunk%4014225

---
+++ src/oaipmh/common.py
@@ -0,0 +1,67 @@
+class Header:
+    def __init__(self, identifier, datestamp, setspec, deleted):
+        self._identifier = identifier
+        self._datestamp = datestamp
+        self._setspec = setspec
+        self._deleted = deleted
+        
+    def identifier(self):
+        return self._identifier
+    
+    def datestamp(self):
+        return self._datestamp
+
+    def setSpec(self):
+        return self._setspec
+
+    def isDeleted(self):
+        return self._deleted
+
+class Metadata:
+    def __init__(self, map):
+        self._map = map
+
+    def getMap(self):
+        return self._map
+    
+    def getField(self, name):
+        return self._map[name]
+
+    __getitem__ = getField
+    
+class ServerIdentify:
+    def __init__(self, repositoryName, baseURL, protocolVersion, adminEmails,
+                 earliestDatestamp, deletedRecord, granularity, compression):
+        self._repositoryName = repositoryName
+        self._baseURL = baseURL
+        self._protocolVersion = protocolVersion
+        self._adminEmails = adminEmails
+        self._earliestDatestamp = earliestDatestamp
+        self._deletedRecord = deletedRecord
+        self._granularity = granularity
+        self._compression = compression
+        # XXX description
+        
+    def repositoryName(self):
+        return self._repositoryName
+
+    def baseURL(self):
+        return self._baseURL
+
+    def protocolVersion(self):
+        return self._protocolVersion
+
+    def adminEmails(self):
+        return self._adminEmails
+
+    def earliestDatestamp(self):
+        return self._earliestDatestamp
+
+    def deletedRecord(self):
+        return self._deletedRecord
+
+    def granularity(self):
+        return self._granularity
+
+    def compression(self):
+        return self._compression
