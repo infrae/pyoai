3c616b4739e246f625d882924582a5ed820a146f
faassen@882ca7fe-3cf8-0310-95c5-f9aabef819dd <none@none>
2006-06-16 13:44:28 +0000

    Add long_description.
    --HG--
    extra : convert_revision : svn%3A882ca7fe-3cf8-0310-95c5-f9aabef819dd/pyoai/trunk%4020170

--- setup.py
+++ setup.py
@@ -12,6 +12,17 @@ The protocol is described here:
 
 http://www.openarchives.org/OAI/openarchivesprotocol.html
 """,
+    long_description="""\
+The oaipmh module is a Python implementation of an "Open Archives
+Initiative Protocol for Metadata Harvesting" (version 2) client and server.
+The protocol is described here:
+
+http://www.openarchives.org/OAI/openarchivesprotocol.html
+
+With it, you can easily harvest existing OAI-PMH compliant data providers,
+as well as construct your own OAI-PMH data provider.
+""",
+    
     packages=find_packages('src', exclude=['*.tests']),
     package_dir = {'': 'src'},
     zip_safe=True,
